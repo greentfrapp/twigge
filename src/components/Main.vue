23884f4317e8b7926a8bb3d5c23937f6caee555d
SK <limsweekiat@gmail.com>
2022-04-12 14:37:24 +0800

    Add replies

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="flex justify-center items-center min-h-screen w-screen overflow-x-hidden">
     <div class="board relative w-max max-w-lg flex flex-col items-center justify-center gap-2">
-      <div class="absolute top-0 h-screen sm:h-[32rem] sm:rounded-xl sm:border flex items-center bg-white">
-        <div class="bg-white w-screen flex flex-col justify-center px-6 sm:px-10 py-4 sm:py-10 gap-6 text-center">
-          <div class="text-prose font-serif text-2xl sm:text-3xl max-w-prose whitespace-pre-wrap text-gray-800">
-            {{ tweets[1].text }}
+      <div class="next-container absolute top-0 h-screen sm:h-[32rem] overflow-auto sm:rounded-xl sm:border flex flex-col items-center bg-white w-[32rem]">
+        <div v-for="(tweet, i) in tweets[1]" :key="i"
+          class="bg-white min-h-screen sm:min-h-full w-screen sm:w-full flex flex-col justify-center px-6 sm:px-10 py-4 sm:py-10 gap-6 text-center z-10">
+          <div class="font-serif text-2xl sm:text-3xl max-w-prose whitespace-pre-wrap text-gray-800">
+            {{ tweet.text }}
           </div>
           <div class="font-serif text-xl text-gray-700">
-            {{ tweets[1].author }}, {{ tweets[1].date }}
+            {{ tweet.author }}, {{ tweet.date }}
           </div>
         </div>
       </div>
-      <div class="tweet h-screen sm:h-[32rem] overflow-auto z-10 shadow-around sm:rounded-xl cursor-pointer flex items-center bg-white">
-        <div class="bg-white w-screen flex flex-col justify-center px-6 sm:px-10 py-4 sm:py-10 gap-6 text-center z-10">
+      <div class="tweet-container h-screen sm:h-[32rem] overflow-auto z-10 shadow-around sm:rounded-xl cursor-pointer flex flex-col divide-y items-center bg-white w-[32rem]">
+        <div v-for="(tweet, i) in tweets[0]" :key="i"
+          class="relative tweet bg-white min-h-screen sm:min-h-full w-screen sm:w-full flex flex-col justify-center px-6 sm:px-10 py-4 sm:py-10 gap-6 text-center z-10">
           <div class="font-serif text-2xl sm:text-3xl max-w-prose whitespace-pre-wrap text-gray-800">
-            {{ tweets[0].text }}
+            {{ tweet.text }}
           </div>
           <div class="font-serif text-xl text-gray-700">
-            {{ tweets[0].author }}, {{ tweets[0].date }}
+            {{ tweet.author }}, {{ tweet.date }}
+          </div>
+          <div v-if="i < (tweets[0].length - 1)" class="absolute bottom-20 sm:bottom-4 right-4">
+            <ChevronDownIcon class="h-6 w-6 text-gray-300 z-50" />
           </div>
         </div>
       </div>
@@ -32,14 +37,34 @@
 </template>
 
 <style scoped>
-.tweet * {
+.tweet-container * {
   touch-action: none;
+}
+
+.tweet-container {
+  scroll-snap-type: y mandatory;
+}
+
+.tweet-container::-webkit-scrollbar,
+.next-container::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.tweet-container,
+.next-container {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+
+.tweet {
+  scroll-snap-align: start;
 }
 </style>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { HeartIcon, RefreshIcon } from '@heroicons/vue/outline'
+import { HeartIcon, RefreshIcon, ChevronDownIcon } from '@heroicons/vue/outline'
 import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/solid'
 import Hammer from 'hammerjs'
 
@@ -47,7 +72,7 @@ export default defineComponent({
   mounted () {
     this.searchTweets()
     this.board = document.querySelector('.board')
-    const tweet = document.querySelector('.tweet') as HTMLElement
+    const tweet = document.querySelector('.tweet-container') as HTMLElement
     this.topCard = tweet
     const hammer = new Hammer(tweet)
     hammer.on('pan', this.panHandler)
@@ -57,20 +82,21 @@ export default defineComponent({
     RefreshIcon,
     CheckCircleIcon,
     XCircleIcon,
+    ChevronDownIcon,
   },
   data () {
     return {
       board: null as any,
       tweets: [
-        {
+        [{
           text: 'The evolution of API for running cutting edge AI:\n- run it on your own machine \n- run it in the cloud\n- apply pay for and query an api endpoint\n- pretty please ask one of the authors to run it for you on Twitter \n🥲',
           author: 'Andrej Karpathy',
           date: 'Apr 8',
-        }, {
+        }], [{
           text: 'Paradox of ‘repairability’ - the choices that make it more reliable also make it harder to repair.',
           author: 'Benedict Evans',
           date: 'Apr 11',
-        },
+        }], 
       ] as any[],
       topCard: null as any,
       isPanning: false,
@@ -89,9 +115,6 @@ export default defineComponent({
       this.topCard.style.opacity = 1
     },
     async updateTweet () {
-      // const tweet = this.tweets[0]
-      // this.tweets[0] = this.tweets[1]
-      // setTimeout(() => this.tweets[1] = tweet, 200)
       setTimeout(() => {
         this.tweets.shift()
         if (this.tweets.length < 4) {
@@ -109,13 +132,17 @@ export default defineComponent({
       }).then(async response => {
         const data = await response.json()
         this.nextToken = data.next_token
-        data.tweets.filter((tweet:any) => tweet.text.split(/\r\n|\r|\n/).length <= 5)
-          .forEach((tweet:any) => {
-            this.tweets.push({
-              text: tweet.text.replace('&gt;', '>').replace('&amp;', '&').replace('&lt;', '<'),
-              author: tweet.author,
-              date: tweet.date,
-            })
+        data.tweets.filter((tweets:any) => tweets[0].text.split(/\r\n|\r|\n/).length <= 5)
+          .forEach((tweets:any) => {
+            this.tweets.push(tweets.filter((tweet:any) => tweet.text.split(/\r\n|\r|\n/).length <= 5)
+              .map((tweet:any) => {
+                return {
+                  text: tweet.text.replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&'),
+                  author: tweet.author,
+                  date: tweet.date,
+                }
+              })
+            )
           })
       })
     },
@@ -178,6 +205,7 @@ export default defineComponent({
               this.topCard.style.transform =
                 'translateX(' + posX + 'px) rotate(' + deg + 'deg)'
               this.topCard.style.opacity = 0
+              this.topCard.scrollTop = 0
             })
         } else if (propX < -0.25 && e.direction === Hammer.DIRECTION_LEFT) {
           this.updateTweet()
@@ -188,6 +216,7 @@ export default defineComponent({
               this.topCard.style.transform =
                 'translateX(' + posX + 'px) rotate(' + deg + 'deg)'
               this.topCard.style.opacity = 0
+              this.topCard.scrollTop = 0
             })
         } else {
           // reset card position
